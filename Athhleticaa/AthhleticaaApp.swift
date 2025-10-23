d2cba97c9fd23d7af1fa74b1ff0ecd49cf07d4a3
Dipanshu <kashyaprohilla543@gmail.com>
2025-10-23 17:28:25 +0530

    Initial Commit

---
+++ Athhleticaa/AthhleticaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthhleticaaApp.swift
+//  Athhleticaa
+//
+//  Created by Dipanshu Kashyap on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AthhleticaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athhleticaa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Athhleticaa
+//
+//  Created by Dipanshu Kashyap on 23/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
